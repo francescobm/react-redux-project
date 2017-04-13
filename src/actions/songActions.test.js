0ef856719233a372b75e205c3b98a2af5c143875
francesco.greppi <francesco.greppi@gmail.com>
2017-04-13 17:04:57 +0100

    Unit tests actions and reducers

---
+++ src/actions/songActions.test.js
@@ -0,0 +1,23 @@
+import expect from 'expect';
+import * as SongActions from './SongActions';
+import * as types from './actionTypes';
+
+//Test a sync action
+describe('Song Actions', ()=>{
+    describe('createSongSuccess', ()=>{
+        it('should create a CREATE_SONG_SUCCESS action', ()=>{
+            //arrange
+            const song = {id: 'enter-sandman', title:'Enter Sandman'};
+            const expectedAction = {
+                type: types.CREATE_SONG_SUCCESS,
+                song: song
+            };
+
+            //act
+            const action = SongActions.createSongSuccess(song);
+
+            //assert
+            expect(action).toEqual(expectedAction);
+        });
+    });
+});
---
+++ src/reducers/songReducer.test.js
@@ -0,0 +1,45 @@
+import expect from 'expect';
+import songReducer from './songReducer';
+import * as actions from '../actions/songActions';
+
+describe('Song Reducer', ()=>{
+    it('should add course when passed CREATE_SONG_SUCCESS',()=>{
+        //arrange
+        const initialState=[
+            {title:'A'},
+            {title:'B'}
+        ];
+        const newSong = {title:'C'};
+        const action = actions.createSongSuccess(newSong);
+
+        //act
+        const newState = songReducer(initialState, action);
+
+        //assert
+        expect(newState.length).toEqual(3);
+        expect(newState[0].title).toEqual('A');
+        expect(newState[1].title).toEqual('B');
+        expect(newState[2].title).toEqual('C');
+    });
+
+    it('should update course when passed UPDATE_SONG_SUCCESS',()=>{
+        //arrange
+        const initialState=[
+            {id:'A', title:'A'},
+            {id:'B', title:'B'}
+        ];
+        const song = {id:'A', title:'Modified A'};
+        const action = actions.updateSongSuccess(song);
+
+        //act
+        const newState = songReducer(initialState, action);
+        const updatedSong = newState.find(a=>a.id == song.id);
+        const untouchedSong= newState.find(a=> a.id == 'B');
+
+        //assert
+        expect(updatedSong.title).toEqual('Modified A');
+        expect(untouchedSong.title).toEqual('B');
+        expect(newState.length).toEqual(2);
+    })
+
+})
